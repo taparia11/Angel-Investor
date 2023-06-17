6f1c55d20c02505696c68927c01c904129107dcb
Yuvraj Devnani <84584971+CoderYuvi@users.noreply.github.com>
2023-06-17 19:48:54 +0530

    added Admin path of investor list

---
+++ routes/admin.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const InvestorSignup = require('../models/InvestorSignup');
+
+// ROUTE 1: Get all the Investors using GET "/api/invest/fetchallinvestors" Login required
+router.get('/fetchallinvestors', async (req, res)=>{
+    try {
+        const Details = await InvestorSignup.find();
+        res.json(Details)    
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
+router.post('/testing', async (req, res)=>{
+
+    try {
+    
+    const{name} = req.body;
+    // if there are error return bad request and error
+
+   
+    res.json(name + ' done') 
+        
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+}
+})
+
+
+module.exports = router
