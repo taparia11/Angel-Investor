a099321bdb976faf5a748fd64cd3120582804da5
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-07-29 01:23:58 +0530

    updated

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const mongoURI = "mongodb+srv://taparia:220807@tapariaji.ohhta.mongodb.net/AngelInvestor"
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
         console.log("connected to Mongo Successfully")
     })
 }
