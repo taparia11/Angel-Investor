a537389f23560c45af4560c8f09a2f8857141010
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-08-03 15:12:22 +0530

    updated

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const InvestorSignup = require('../models/InvestorSignup');
 
 // ROUTE 1: Get all the Investors using GET "/api/invest/fetchallinvestors" Login required
-router.get('/fetchallinvestors', async (req, res)=>{
+router.get('/fetchallinvestors', async (req, res) => {
     try {
         const Details = await InvestorSignup.find();
-        res.json(Details)    
+        res.json(Details)
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
@@ -14,20 +14,17 @@ router.get('/fetchallinvestors', async (req, res)=>{
 })
 
 // ROUTE 2: fTo test the backend and API
-router.post('/testing', async (req, res)=>{
+router.post('/testing', async (req, res) => {
 
     try {
-    
-    const{name} = req.body;
-    // if there are error return bad request and error
+        const { name } = req.body;
+        // if there are error return bad request and error
+        res.json(name + ' done')
 
-   
-    res.json(name + ' done') 
-        
-} catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
-}
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 
